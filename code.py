3e932eaa436ff8741eafdbd7e26adcbeed187122
Lukas Parsons <lukasparsons2013@gmail.com>
2021-07-14 03:54:57 -0400

    removed hardcoding, updated code to bluetooth

--- code.py
+++ code.py
@@ -1,20 +1,26 @@
-from adafruit_circuitplayground.express import cpx
-import usb_cdc
+from adafruit_circuitplayground.bluefruit import cpb
+
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
+
 import time
 
-sp = usb_cdc.serials[1]
-sp.timeout = 0
-cpx.pixels.brightness = 0.009
-cpx.pixels.fill((0, 0, 0))
-cpx.pixels.show()
+ble = BLERadio()
+uart = UARTService()
+advertisement = ProvideServicesAdvertisement(uart)
+
+cpb.pixels.brightness = 0.009
+cpb.pixels.fill((0, 0, 0))
+cpb.pixels.show()
 
 messagequeue = []
 
-lycaonpixels = [0, 1]
-astridpixels = [2, 3]
-resolutepixels = [4, 5]
-adonispixels = [6, 7]
-tipixels = [8, 9]
+set0 = [0, 1] # lycaon
+set1 = [2, 3] # astrid
+set2 = [4, 5] # resolute
+set3 = [6, 7] # adonis
+set4 = [8, 9] # ti
 
 BLACK = (0,0,0)
 RED = (255, 0, 0)
@@ -25,112 +31,120 @@ BLUE = (0, 0, 255)
 PURPLE = (180, 0, 255)
 ORANGE = (255, 69, 0)
 
-def activatePlayerLEDs(pixels, color):
+def activateLEDs(pixels, color):
 	for p in pixels:
-		cpx.pixels[p] = color
+		cpb.pixels[p] = color
 
 def turnOffAll():
-	cpx.pixels.fill(RED)
-	cpx.pixels.fill((0,0,0))
+	cpb.pixels.fill(RED)
+	cpb.pixels.fill((0,0,0))
 
 def turnOffRecent():
 	for p in messagequeue[len(messagequeue)-1]:
-		cpx.pixels[p] = BLACK
+		cpb.pixels[p] = BLACK
 	del messagequeue[len(messagequeue)-1]
 	time.sleep(0.5)
 
 def breathe():
-	activatePlayerLEDs(resolutepixels, PURPLE)
+	activateLEDs(set2, PURPLE)
 
 def handleMessage(s):
-	if s == 'resolute-du-coreiseuse':
-		activatePlayerLEDs(resolutepixels, PURPLE)
-		if resolutepixels in messagequeue:
-			messagequeue.append(messagequeue.pop(messagequeue.index(resolutepixels)))
+	if s == 'set0':
+		activateLEDs(set0, BLUE)
+		if set0 in messagequeue:
+			messagequeue.append(messagequeue.pop(messagequeue.index(set0)))
 		else:
-			messagequeue.append(resolutepixels)
-	if s == 'adonis-jahran':
-		activatePlayerLEDs(adonispixels, ORANGE)
-		if adonispixels in messagequeue:
-			messagequeue.append(messagequeue.pop(messagequeue.index(adonispixels)))
+			messagequeue.append(set0)
+	if s == 'set1':
+		activateLEDs(set1, GREEN)
+		if set1 in messagequeue:
+			messagequeue.append(messagequeue.pop(messagequeue.index(set1)))
 		else:
-			messagequeue.append(adonispixels)
-	if s == 'lycaon-ascelin':
-		activatePlayerLEDs(lycaonpixels, BLUE)
-		if lycaonpixels in messagequeue:
-			messagequeue.append(messagequeue.pop(messagequeue.index(lycaonpixels)))
+			messagequeue.append(set1)
+	if s == 'set2':
+		activateLEDs(set2, PURPLE)
+		if set2 in messagequeue:
+			messagequeue.append(messagequeue.pop(messagequeue.index(set2)))
 		else:
-			messagequeue.append(lycaonpixels)
-	if s == 'ti-bronsson':
-		activatePlayerLEDs(tipixels, YELLOW)
-		if tipixels in messagequeue:
-			messagequeue.append(messagequeue.pop(messagequeue.index(tipixels)))
+			messagequeue.append(set2)
+	if s == 'set3':
+		activateLEDs(set3, ORANGE)
+		if set3 in messagequeue:
+			messagequeue.append(messagequeue.pop(messagequeue.index(set3)))
 		else:
-			messagequeue.append(tipixels)
-	if s == 'astrid':
-		activatePlayerLEDs(astridpixels, GREEN)
-		if astridpixels in messagequeue:
-			messagequeue.append(messagequeue.pop(messagequeue.index(astridpixels)))
+			messagequeue.append(set3)
+	if s == 'set4':
+		activateLEDs(set4, YELLOW)
+		if set4 in messagequeue:
+			messagequeue.append(messagequeue.pop(messagequeue.index(set4)))
 		else:
-			messagequeue.append(astridpixels)
-	if s == 'Test Message':
-		cpx.pixels.fill(GREEN)
+			messagequeue.append(set4)
+
 	if s == 'test-channel':
-		activatePlayerLEDs(resolutepixels, PURPLE)
-		activatePlayerLEDs(adonispixels, ORANGE)
-		activatePlayerLEDs(lycaonpixels, BLUE)
-		activatePlayerLEDs(tipixels, YELLOW)
-		activatePlayerLEDs(astridpixels, GREEN)
+		activateLEDs(set0, BLUE)
+		activateLEDs(set1, GREEN)
+		activateLEDs(set2, PURPLE)
+		activateLEDs(set3, ORANGE)
+		activateLEDs(set4, YELLOW)		
 
 def getColorFromPixels(pix):
-	if pix == resolutepixels:
-		return PURPLE
-	if pix == adonispixels:
-		return ORANGE
-	if pix == lycaonpixels:
+	if pix == set0:
 		return BLUE
-	if pix == tipixels:
-		return YELLOW
-	if pix == astridpixels:
+	if pix == set1:
 		return GREEN
+	if pix == set2:
+		return PURPLE
+	if pix == set3:
+		return ORANGE
+	if pix == set4:
+		return YELLOW
+	
 
 while True:
-	if cpx.button_a:
-		turnOffAll()
+	ble.start_advertising(advertisement)
+	while not ble.connected:
+		pass
 
-	if cpx.button_b:
-		if messagequeue:
-			turnOffRecent()
-		else:
+	ble.stop_advertising()
+	print("CONNECTED")
+
+	while ble.connected:
+
+		if uart.in_waiting:
+			raw_bytes = uart.read(uart.in_waiting)
+			text = raw_bytes.decode().strip()
+			print("RX: ", text)
+			handleMessage(text)
+
+		if cpb.button_a:
 			turnOffAll()
-	
-	if cpx.touch_A3:
-		for p in tipixels:
-			cpx.pixels[p] = BLACK
-	
-	if cpx.touch_A1:
-		for p in adonispixels:
-			cpx.pixels[p] = BLACK
 
-	if cpx.touch_A7:
-		for p in resolutepixels:
-			cpx.pixels[p] = BLACK
-	
-	if cpx.touch_A6:
-		for p in astridpixels:
-			cpx.pixels[p] = BLACK
+		if cpb.button_b:
+			if messagequeue:
+				turnOffRecent()
+			else:
+				turnOffAll()
+		
+		if cpb.touch_A3:
+			for p in set4:
+				cpb.pixels[p] = BLACK
+		
+		if cpb.touch_A1:
+			for p in set3:
+				cpb.pixels[p] = BLACK
 
-	if cpx.touch_A4:
-		for p in lycaonpixels:
-			cpx.pixels[p] = BLACK
+		if cpb.touch_TX:
+			for p in set2:
+				cpb.pixels[p] = BLACK
+		
+		if cpb.touch_A6:
+			for p in set1:
+				cpb.pixels[p] = BLACK
 
-	if sp.in_waiting > 0:
-		b = sp.readline(-1)
-		if b is not None:
-			bs = b.decode('utf-8')
-			print(bs)
-			handleMessage(bs)
+		if cpb.touch_A4:
+			for p in set0:
+				cpb.pixels[p] = BLACK
+		
+		cpb.pixels.show()
 
-
-
-	cpx.pixels.show()
+	print("DISCONNECTED")
